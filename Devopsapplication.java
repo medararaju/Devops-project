9ff26899c19f38b8f03c657e04fdcfad7a9b0468
root <root@ip-172-31-88-37.ec2.internal>
2022-04-20 12:14:27 +0000

    second-message

--- Devopsapplication.java
+++ Devopsapplication.java
@@ -1,4 +1,36 @@
 here we are going to learn the devops contennt from the pravin redyy
 
 
-welcomde to dedvops learning
+welcomde to dedvops learninif weare 
+
+
+if we are going to learn the 
+
+first need to login the server.
+
+then go with the ssh-keygen
+it will create the key pub and private
+
+then go withlocation /root/.ssh/
+
+ls /root/.ssh/ib_pub/
+
+then copy the file  and go with remote server and go to setings and cretae ssh&genkey
+
+then copy the key
+cretate a ssh file
+the go with rpositories
+get the ssh address 
+go with ubuntu
+
+type commond like git clone address
+
+tthen commond ls
+the cd chnge
+thne cretae the file 
+then save
+then check the ststus
+git add project name
+git commit
+git push origin master
+
